36887d8e60410980ecc59c69c161d30935509448
DilaraGule <80154346+DilaraGule@users.noreply.github.com>
2022-02-15 15:23:22 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace tip_donusumleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implicit Conversion (Bilinçsiz Dönüşüm)
+            //Bir değişkenin kendinden daha yüksek kapasiteli bir değişkene atanması
+
+            byte a = 5;
+            sbyte b = 30;
+            short c = 10;
+
+            int d = a + b + c;
+
+            // Explicit Conversion (Bilinçli Dönüşüm)
+            // parse, convert, toString
+            int x = 4;
+            byte y = (byte)x;
+
+            int xx = 6;
+            string yy= xx.ToString();
+            
+        }
+    }
+}
